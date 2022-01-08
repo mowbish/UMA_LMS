72237eefc980091969b75408fa21b75b63e3d07e
mowbish <mobin.clash.ma@gmail.com>
2022-01-08 23:21:02 +0330

    Changed base app name from UMA_LMS to conf

--- conf/urls.py
+++ conf/urls.py
@@ -1,4 +1,4 @@
-"""UMA_LMS URL Configuration
+"""conf URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
