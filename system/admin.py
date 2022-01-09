4e969a351261b850dc15fbfee12060a4e5726d78
mowbish <mobin.clash.ma@gmail.com>
2022-01-09 10:39:29 +0330

    Added models to admin.py file

--- system/admin.py
+++ system/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import (Collage, Faculty, Class, Person, Professor, Student, Lesson, Task)
 
-# Register your models here.
+admin.site.register(Collage)
+
+
+class CollageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Faculty)
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Class)
+
+
+class ClassAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Person)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Professor)
+
+
+class ProfessorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Student)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Lesson)
+
+
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Task)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    pass
