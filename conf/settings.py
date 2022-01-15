31578693a6db2fe7ecafb6cfbd5dc1b676de3ddd
mowbish <mobin.clash.ma@gmail.com>
2022-01-15 12:41:47 +0330

    Added new app to installed apps

--- conf/settings.py
+++ conf/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "system",
+    'system',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
