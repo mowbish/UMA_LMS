9a91671d447cb7a2166f96f66a2d62aa66e51a86
mowbish <mobin.clash.ma@gmail.com>
2022-01-15 18:32:27 +0330

    Removed AUTH_USER_MODEL from setting because abstruct user removed from models.py and we use User insted of that

--- conf/settings.py
+++ conf/settings.py
@@ -120,4 +120,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "system.Person"
