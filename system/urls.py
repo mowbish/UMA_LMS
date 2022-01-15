86902a5378158b72f47e237bc34ecddc5d95e345
mowbish <mobin.clash.ma@gmail.com>
2022-01-15 18:34:42 +0330

    New paths for urls

--- system/urls.py
+++ system/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import ShowCollagesAPIView, ShowFacultyAPIView, CreateStudentAPIView
 
 urlpatterns = [
+    path('collages/', ShowCollagesAPIView.as_view({'get': 'list'})),
+    path('faculties/', ShowFacultyAPIView.as_view({'get': 'list'})),
+    path('signup/student/', CreateStudentAPIView.as_view()),
+    path('signup/professor/', CreateStudentAPIView.as_view()),
 ]
