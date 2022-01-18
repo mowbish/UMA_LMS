d7e85f8480eaad0d1fdc9b191f4b29493dc0af7a
Mobin <mobin.clash.ma@gmail.com>
2022-01-18 10:55:42 +0330

    Added SignUp path for professors and students

--- system/urls.py
+++ system/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ShowCollagesAPIView, ShowFacultyAPIView, CreateStudentAPIView
+from .views import ShowCollagesAPIView, ShowFacultyAPIView, CreateStudentAPIView, CreateProfessorAPIView, \
+    CreateLessonAPIView
 
 urlpatterns = [
     path('collages/', ShowCollagesAPIView.as_view({'get': 'list'})),
     path('faculties/', ShowFacultyAPIView.as_view({'get': 'list'})),
     path('signup/student/', CreateStudentAPIView.as_view()),
-    path('signup/professor/', CreateStudentAPIView.as_view()),
+    path('signup/professor/', CreateProfessorAPIView.as_view()),
+    path('create/class/', CreateLessonAPIView.as_view()),
 ]
