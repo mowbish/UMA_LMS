b3c5d7c1d3664c87196e1dd3fc234c7e0e5e873c
Mobin <mobin.clash.ma@gmail.com>
2022-01-19 18:14:22 +0330

    Changes on urls

--- system/urls.py
+++ system/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls.static import static
+from conf import settings
 from .views import ShowCollagesAPIView, ShowFacultyAPIView, CreateStudentAPIView, CreateProfessorAPIView, \
-    CreateLessonAPIView, CreateRoomAPIView, show_rooms, room_detail
+    CreateLessonAPIView, CreateRoomAPIView, show_rooms, room_detail, FileUploadView
 
 urlpatterns = [
     path('collages/', ShowCollagesAPIView.as_view({'get': 'list'})),
@@ -11,6 +13,9 @@ urlpatterns = [
     path('create/room/', CreateRoomAPIView.as_view()),
 
     path('rooms/', show_rooms, name="rooms"),
-    path('rooms/<slug:slug>', room_detail, name="room_detail"),
+    path('rooms/<slug:slug>/', room_detail, name="room_detail"),
+    path('upload/content/', FileUploadView, name="upload_content"),
     # path('rooms/', go_to_room, name="room_detail"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
